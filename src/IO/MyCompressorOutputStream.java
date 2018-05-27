85f4e6973f57b8ded6b0a3e2742b5992e60c3647
lorentsa <lorentsa@post.bgu.ac.il>
2018-05-27 12:45:49 +0300

    first update maze

---
+++ src/IO/MyCompressorOutputStream.java
@@ -0,0 +1,19 @@
+package IO;
+
+import java.io.OutputStream;
+
+public class MyCompressorOutputStream extends OutputStream {
+    OutputStream out;
+
+    public MyCompressorOutputStream(OutputStream o) {
+
+    }
+
+    public void write(int b){
+
+
+
+
+    }
+
+}
---
+++ src/IO/MyDecompressorInputStream.java
@@ -0,0 +1,14 @@
+package IO;
+
+import java.io.InputStream;
+
+public class MyDecompressorInputStream extends InputStream {
+
+    public MyDecompressorInputStream(){
+
+    }
+
+    public int read(){
+        return 0;
+    }
+}
